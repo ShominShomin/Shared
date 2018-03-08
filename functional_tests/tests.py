391c18349f34a315f745ef869ad81eb6efd82c1b
Battuvshin <battuvshin.neet@gmail.com>
2018-03-08 16:56:39 +0800

    added first app core

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -19,7 +19,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url)
-
         self.assertIn('To-Do', self.browser.title)
+
+
         header_text = self.browser.find_element_by_tag_name('h1').text
+
         self.assertIn('To-Do', header_text)
