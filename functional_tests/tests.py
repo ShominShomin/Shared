e1bc5b4986f81228a6d756df6fc991f080a4fe70
Battuvshin <battuvshin.neet@gmail.com>
2018-03-08 16:43:34 +0800

    Setup Environment

---
+++ functional_tests/tests.py
@@ -0,0 +1,25 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+
+import time
+import unittest
+
+MAX_WAIT = 10
+
+class NewVisitorTest(StaticLiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.refresh()
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get(self.live_server_url)
+
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
