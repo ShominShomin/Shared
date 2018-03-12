c4d03aff5c6c8a22d61c23c22291d7563fb03362
Battuvshin <battuvshin.neet@gmail.com>
2018-03-13 05:33:56 +0800

    Styled and generated calendar.

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.utils.safestring import mark_safe
-from calendar import HTMLCalendar
+from calendar import HTMLCalendar, month_name
 from datetime import date
 from itertools import groupby
 from .models import Item
@@ -49,7 +49,7 @@ class WorkoutCalendar(HTMLCalendar):
                 body.append('</ul>')
                 return self.day_cell(cssclass, '%d %s' % (day, ''.join(body)))
             return self.day_cell(cssclass, day)
-        return self.day_cell('noday', '&nbsp;')
+        return self.day_cell('noday', '')
 
     def formatmonth(self, year, month):
         self.year, self.month = year, month
@@ -57,15 +57,22 @@ class WorkoutCalendar(HTMLCalendar):
 
     def group_by_day(self, workouts):
         field = lambda workout: workout.my_date.day
-        return dict(
-            [(day, list(items)) for day, items in groupby(workouts, field)])
+        return dict([(day, list(items)) for day, items in groupby(workouts, field)])
 
     def day_cell(self, cssclass, body):
         return '<td class="%s">%s</td>' % (cssclass, body)
+
+    def formatmonthname(self, theyear, themonth, withyear=True):
+        if withyear:
+            s = '%s %s' % (month_name[themonth], theyear)
+        else:
+            s = '%s' % month_name[themonth]
+        return '<tr><th colspan="7" class="monthName">%s</th></tr>' % s
+
 
 def calendar(request, year, month):
     year = int(year)
     month = int(month)
     my_workouts = Item.objects.order_by('my_date').filter(my_date__year=year,    my_date__month=month)
     cal = WorkoutCalendar(my_workouts).formatmonth(year, month)
-    return render_to_response('base.html', {'calendar': mark_safe(cal),})
+    return render_to_response('reservation.html', {'calendar': mark_safe(cal),})
